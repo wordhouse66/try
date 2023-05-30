4c347c0ee27cce7f8c8dab9bbef66a3987b68d60
wordhouse66 <wordhouse66@yahoo.com>
2023-05-30 14:07:04 +0100

    design home page and add links, navbar and footer

--- Gemfile
+++ Gemfile
@@ -46,6 +46,10 @@ gem "bootsnap", require: false
 
 # Use Sass to process CSS
 # gem "sassc-rails"
+gem 'bootstrap', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'sassc-rails', '>= 2.1.0'
+
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[ show edit update destroy ]
 
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -13,7 +13,6 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
@@ -27,7 +26,6 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = @article = Article.find(params[:id])
     if @article.update(article_params)
       redirect_to article_url(@article), notice: 'Article successfully updated'
     else
@@ -36,13 +34,17 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = 'Article successfully destroyed'
     redirect_to articles_url
   end
 
   private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
   def article_params
     params.require(:article).permit(:title, :description)
   end
