09fa4b911ccb5bd86be5b1860a12064d6bec3a1f
wordhouse66 <wordhouse66@yahoo.com>
2023-06-03 20:26:05 +0100

    update surrentuser tobe article creator

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,7 +19,8 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = @user.articles.new(article_params)
+    @article = Article.new(article_params)
+    @article.user = current_user
     if @article.save
       flash[:success] = 'Article successfully created'
       redirect_to article_path(@article)
