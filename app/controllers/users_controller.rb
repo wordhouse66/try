6d89525bd1c1127e7496a466d68aaa8f6c9503c6
wordhouse66 <wordhouse66@yahoo.com>
2023-06-04 08:01:15 +0100

    Add sign in upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,8 +17,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the Alpha Bog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render :new, status: :unprocessable_entity
     end
