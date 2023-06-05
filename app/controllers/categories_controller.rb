f80a631d4128580f0692a6fc0808ace2c316dc7b
wordhouse66 <wordhouse66@yahoo.com>
2023-06-05 19:36:41 +0100

    add edit category feature

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :require_admin, except: [:index, :show]
+  before_action :set_category, only: [:show, :edit, :update]
 
   def index
     @categories = Category.paginate(page: params[:page])
@@ -20,11 +21,27 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    @category = Category.find(params[:id])
     @category_articles = @category.articles.paginate(page: params[:page])
   end
 
+  def edit
+
+  end
+
+  def update
+    if @category.update(category_params) 
+      flash[:success] = 'Category successfully updated'
+      redirect_to category_url(@category)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
 
   def category_params
     params.require(:category).permit(:name)
